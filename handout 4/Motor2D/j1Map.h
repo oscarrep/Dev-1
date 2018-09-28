7ff8cb0edc8d8447bfead28391c7e6ab50ffa384
oscarrep <oscar.bcn.95@gmail.com>
2018-09-28 02:18:08 +0200

    TODO 1, 2

--- handout 4/Motor2D/j1Map.h
+++ handout 4/Motor2D/j1Map.h
@@ -9,33 +9,42 @@
 // TODO 2: Create a struct to hold information for a TileSet
 // Ignore Terrain Types and Tile Types for now, but we want the image!
 // ----------------------------------------------------
-struct TileSet
+struct tile_set
 {
 	uint tilewidth = 0;
 	uint tileheight = 0;
 	uint spacing = 0;
 	uint margin = 0;
+	char* archivedirection = nullptr;
 };
 
 // TODO 1: Create a struct needed to hold the information to Map node
 
-enum class map_orientation
+enum class render_order
 {
 	right_down,
 	left_down,
 	right_up,
 	left_up,
-	error
+	none
 };
 
-struct Mapinfo
+enum class map_orientation
 {
-	//char* version;
-	uint width  = 0;
-	uint height = 0;
-	uint tilewidth;
-	uint tileheight;
-	//map_orientation orientation = map_orientation::error;
+	orthogonal,
+	isometric,
+	none
+};
+
+struct map_info
+{
+	map_orientation orientation = map_orientation::none;
+	render_order order = render_order::none;
+	uint tilewidth = 0;
+	uint tileheight = 0;
+	uint spacing = 0;
+	uint margin = 0;
+
 };
 
 // ----------------------------------------------------
@@ -67,7 +76,8 @@ public:
 
 	// TODO 1: Add your struct for map info as public for now
 
-	Mapinfo map1;
+	map_info map1;
+	tile_set info_tile;
 	
 private:
 

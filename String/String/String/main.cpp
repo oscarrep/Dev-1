fdd8c0a3bdef1945055204f7ac3456c41ec589cf
oscarrep <oscar.bcn.95@gmail.com>
2018-10-02 20:44:59 +0200

    redoing string

--- String/String/String/main.cpp
+++ String/String/String/main.cpp
@@ -1,17 +1,3 @@
 #include "string.h"
 #include <stdlib.h>
 
-int main()
-{
-	String a;
-	String b(a);
-	String c("Hello");
-
-	a = "hi";
-
-	if (a == "bye")
-		return true;
-
-	system("pause");
-	return 0;
-}
